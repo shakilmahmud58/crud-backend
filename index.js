97ee653ed1274d356d27415c8181958b1c467987
Shakil Mahmud <almahmudruet17@gmail.com>
2022-04-25 14:52:16 +0600

    add env file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require('cors');
 const bodyParser= require('body-parser');
+require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:false}));
